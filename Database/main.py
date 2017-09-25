b2b1d70d8e67fa965da0dc4fcd28e6e72b70d1fd
vishwanathavin <vishwanathavin@gmail.com>
2017-09-25 17:23:09 +0530

    validate the data before going to database

--- Database/main.py
+++ Database/main.py
@@ -1,15 +1,33 @@
 import pymongo
 import pandas as pd
 import json
+import codecs
+import ast
 
 def adjustStartup(db):
-    startupFile = pd.read_csv('./data/startupData.csv')
-    records = json.loads(startupFile.T.to_json()).values()
 
-    db.trial.insert(records)
+    sampleData = pd.read_csv(codecs.open('./data/sampleData.csv', "r", encoding='utf-8',errors='ignore'))
+
+    sampleData['roundInvestmentAmountINR'] =sampleData['roundInvestmentAmountINR'].apply(ast.literal_eval)
+    sampleData['investorID'] =sampleData['investorID'].apply(ast.literal_eval)
+    sampleData['founderID'] =sampleData['founderID'].apply(ast.literal_eval)
+    sampleData['startupClassification'] =sampleData['startupClassification'].apply(ast.literal_eval)
+    # sampleData['dealDate'] =sampleData['dealDate'].apply(ast.literal_eval)
+    sampleData['groupClassification'] =sampleData['groupClassification'].apply(ast.literal_eval)
+
+    for index,row in sampleData.iterrows():
+        for column in sampleData:
+            # print type(sampleData.iloc[0][column]),sampleData.iloc[0][column]
+
+            if isinstance(sampleData.iloc[0][column], list):
+                for x in range(len(sampleData.iloc[0][column])):
+                    print column,': ',x,': ',sampleData.iloc[0][column][x], type(sampleData.iloc[0][column][x])
+            else:
+                print column,': ',(sampleData.iloc[0][column]), type(sampleData.iloc[0][column])
+
     # get the city in the right format
 
-    # Set validators
+    # Do the checks for format locally
     # Push
     print "Do nothing"
 def adjustPerson():
@@ -19,15 +37,15 @@ def adjustPerson():
     print "DO nothing"
 
 def main():
-    try:
-        conn = pymongo.MongoClient()
-        print "Connected successfully!!!"
-    except pymongo.errors.ConnectionFailure, e:
-        print "Could not connect to MongoDB: %s" % e
+    # try:
+    #     conn = pymongo.MongoClient()
+    #     print "Connected successfully!!!"
+    # except pymongo.errors.ConnectionFailure, e:
+    #     print "Could not connect to MongoDB: %s" % e
+    #
+    # db = conn.YNOS
 
-    db = conn.YNOS
-
-    adjustStartup(db)
+    adjustStartup(db=None)
     # adjustPerson(db)
 if __name__=="__main__":
     main()
